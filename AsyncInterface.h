896749d2d1c9769ec0e1fa7c82be5d8532bb9b92
nemasu <nemasu@gmail.com>
2015-01-21 16:21:44 +0900

    Added vdtor to PacketParser

--- AsyncInterface.h
+++ AsyncInterface.h
@@ -21,6 +21,8 @@ class PacketParser {
 
 		//Returns serialization of pkt, size is written into out_size.
 		virtual char*        serialize   ( Packet         *pkt,    unsigned int *out_size  ) = 0;
+
+		virtual ~PacketParser();
 };
 
 #endif
